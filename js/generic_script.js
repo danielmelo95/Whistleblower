112a80aa546e87a36111c5dd5d689df0231bdfa2
danielmelo95 <melo.d1995@gmail.com>
2017-11-30 15:49:10 +0100

    localstorage done

--- js/generic_script.js
+++ js/generic_script.js
@@ -1,4 +1,7 @@
 var current_div;
+var currentState;
+var states=[];
+//drag and drop source: https://www.w3schools.com/html/html5_draganddrop.asp
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -8,6 +11,9 @@ function drag(ev) {
 
 }
 function drop(ev) {
+    console.log('1');
+    saveState();
+
     /*var current_string = nodeToString(current_div);
     //console.log(current_string);
     var letter_div = document.getElementById("recycled_letters");
@@ -29,21 +35,37 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));*/
     var hlp_var = null;
     if(ev.target.className != "crossword_box"){
-        console.log("A");
+        //console.log("A");
         hlp_var = ev.target.parentNode;
+        //console.log(ev.target);
+        document.getElementById("letters_div").innerHTML+= nodeToString(ev.target);
         ev.target.parentNode.removeChild(ev.target);
     }
+/*
     ev.preventDefault();
     var data=ev.dataTransfer.getData("Text");
+*/
 
+
+    //hlp_var= null;
+    //hlp_var= null;
+    //console.log(ev);
+    //ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    //ev.target.appendChild(document.getElementById(data));
     if(hlp_var!=null){
-        hlp_var.appendChild(document.getElementById(data).cloneNode(true));
+        //hlp_var.appendChild(document.getElementById(data).cloneNode(true));
+        hlp_var.appendChild(document.getElementById(data));
+        //console.log(document.getElementById(data));
     }
     else{
-        ev.target.appendChild(document.getElementById(data).cloneNode(true));
-    }
+        //ev.target.appendChild(document.getElementById(data).cloneNode(true));
+        ev.target.appendChild(document.getElementById(data));
+        //console.log(document.getElementById(data));
 
-    hlp_var= null;
+    }
+    saveToLocalStorage(document.getElementById("body"));
+
 }
 
 function nodeToString ( node ) {
@@ -56,10 +78,143 @@ function nodeToString ( node ) {
 
 function validate_crossword(){
     //create array of strings from crossword
+    //console.log("AAA");
+    var childNodes = 1;
+    var crossword_div= document.getElementById("crossword");
+    //console.log(crossword_div);
+    var crossword_childs = crossword_div.childNodes;
+    //console.log(crossword_childs);
+    var str='';
+    var noaccStr;
+    var checkvar= 0;
+    var filledCrossword = [];
 
-    //console.log("aaaa");
-    //compare array of strings from xml with array of strings from crossword
-    for(var i=0; i<final_result.length; i++){
 
+    /*for(var i=0; i<solutions.length; i++){
+        //console.log(solutions[i]);
+        var current_sol = solutions[i];
+
+
+//      console.log(crossword_childs[8]);
+        */
+    for(var j=0; j<crossword_childs.length; j++) {
+
+        if (!crossword_childs[j].hasChildNodes()) {
+            alert("Fill out all spaces");
+            return;
+        }
     }
+            /*str+=''+crossword_childs[childNodes].firstChild.title;
+            childNodes++;*/
+            //console.log(childNodes);
+//     }*/
+    for(var j=0; j<crossword_childs.length; j++){
+        //console.log(crossword_childs[j].id + "aaa");
+
+        if(crossword_childs[j].id=="actual_crossword"){
+            filledCrossword.push(crossword_childs[j].childNodes[0].title);
+            //console.log(filledCrossword= crossword_childs[j].childNodes[0].title);
+        }
+    }
+    //console.log(filledCrossword);
+
+    for(var i=0; i<crosswordSolution; i++){
+        if(crosswordSolution[i]!=filledCrossword[i]){
+            checkvar = 1;
+        }
+        else {
+            checkvar=0;
+        }
+    }
+        childNodes+=2;
+        //console.log(childNodes);
+
+       /* if(current_sol == str){
+            checkvar = 0;
+        }
+        else{
+            checkvar = 1;
+        }*/
+
+
+
+    if(checkvar == 0){
+        alert("Well done, crossword done...");
+
+        current_level++;
+        document.getElementById("crossword").innerHTML = '';
+        myFunction(1);    // 1- reset was pressed, do not chceck local storage
+   }
+    else if(checkvar == 1){
+        alert("Something is wrong with your crossword, consider conrrection...");
+    }
+
+}
+
+function resetGame() {
+    document.getElementById("points_div").innerHTML='Points: '+'';
+    document.getElementById("crossword").innerHTML = '';
+    document.getElementById("letters_div").innerHTML = '';
+    localStorage.clear();
+    myFunction(1);
+}
+
+function saveState() {
+   // createCookie(nodeToString(document.getElementById("body")));
+    /*var d = new Date();
+    d.setTime(d.getTime() + (365*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+
+    document.cookie = "body=" + nodeToString(document.getElementById("body")) + ";" + expires + ";path=/";
+    //document.cookie = "body=" + nodeToString(document.getElementById("body"));
+    //console.log(document.cookie + "AA");*/
+    states.push(nodeToString(document.getElementById("body")));
+   // console.log(states[states.length-1]);
+    if(states.length>3){
+        states.shift();
+        //console.log(states);
+        //states.length=3;
+    }
+    // console.log(document.getElementById("body").childNodes);
+}
+
+function goBackOneStep() {
+    if(states.length==0){
+        alert("You can not go further back, sorry");
+        return;
+    }
+
+    document.getElementById("body").innerHTML='';
+   // console.log(states[states.length-1]);
+   document.getElementById("body").innerHTML += states[states.length-1];
+    states.pop();
+    saveToLocalStorage(document.getElementById("body"));
+
+}
+
+function showDate(){
+    n =  new Date();
+    y = n.getFullYear();
+    m = n.getMonth() + 1;
+    d = n.getDate();
+    //console.log(m + "/" + d + "/" + y);
+    document.getElementById("date").innerHTML = d + "/" + m + "/" + y;
+    //enableTouchmovement();
+
+
+}
+
+function enableTouchmovement(){
+    $('.letters_cls').draggable();
+    $('.letters_cls').sortable();
+    $('.letters_cls').disableSelection();
+    $( ".crossword_box" ).droppable({
+        drop: function( event, ui ) {
+            $( this )
+                .addClass( "ui-state-highlight" )
+                .find( "div" )
+            //    .html( "Dropped!" );
+        }
+    });
+
 }
--- js/xml_loader.js
+++ js/xml_loader.js
@@ -1,46 +1,108 @@
 var solutions = [];
 var final_result;
+var current_level=0;
+var arrayOfLetters = [];
+var points = 0;
+var cookieVar="";
+var xmlDoc;
+var crosswordSolution;
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
+        xml=this;
+        xmlDoc = xml.responseXML;
+        myFunction(0);              // 0 - chcek local storage
     }
 };
 xhttp.open("GET", "../xml/krizovka.xml", true);
 xhttp.send();
 
-function myFunction(xml) {
-    var xmlDoc = xml.responseXML;
-    var result = xmlDoc.getElementsByTagName("tajnicka")[0].childNodes[0].nodeValue;
-    console.log(result);
-    var help = xmlDoc.getElementsByTagName("napoveda")[0].childNodes[0].nodeValue;
-    console.log(help);
-    var solution = xmlDoc.getElementsByTagName("riesenie")[0].childNodes[0].nodeValue;
-    console.log(solution);
-    for(var i=0; i<result.length; i++ ) {
-        var demoHTML = document.getElementById("crossword");
-        /*document.getElementById("demo").innerHTML += xmlDoc.getElementsByTagName("napoveda")[i].childNodes[0].nodeValue+'<br>'+
-            xmlDoc.getElementsByTagName("riesenie")[i].childNodes[0].nodeValue+'<br>';*/
-        var sol1 = xmlDoc.getElementsByTagName("riesenie")[i].childNodes[0].nodeValue;
-        var found_match = 0;
-        demoHTML.innerHTML += '<div class = "outer_div"></div>';
-        solutions.push(sol1);
-        final_result = result;
-        for(var j=0; j<sol1.length; j++){
-            if(result[i]==sol1[j] && found_match!=1) {
-                console.log(result[i]);
-                found_match = 1;
-                demoHTML.innerHTML += '<div class = "crossword_box" id = "actual_crossword" ondrop="drop(event)"' +
-                    ' ondragover="allowDrop(event)"></div>';
-            }
-            else{
-                demoHTML.innerHTML += '<div class = "crossword_box"ondrop="drop(event)" ondragover="allowDrop(event)"></div>';
-            }
-            if(j==sol1.length-1){
-                demoHTML.innerHTML +=xmlDoc.getElementsByTagName("napoveda")[i].childNodes[0].nodeValue;
-            }
+function myFunction(control) {
+    if (control == 0) {
+        var chceck = checkLocalStorage();
+        if(chceck != null){
+            console.log("AAAA");
+            states = [];
+            document.getElementById("body").innerHTML = chceck;
         }
-        found_match = 0;
+        else{
+            control= 1;
+        }
     }
+    if (control == 1) {
+        localStorage.clear();
+        console.log("AAABBB");
+        states = [];
+        document.getElementById("points_div").innerHTML = 'Points: ';
+        arrayOfLetters = [];
+        document.getElementById("letters_div").innerHTML = '';
+        sol1 = '';
+        solutions = [];
+        var crossword = xmlDoc.getElementsByTagName("krizovka");
+        //console.log(crossword[current_level]);
+        var curent_crosswrord = crossword[current_level];
+        var result = curent_crosswrord.getElementsByTagName("tajnicka")[0].childNodes[0].nodeValue;
+        //console.log(result);
+        crosswordSolution = result;
+        //console.log(result);
+        var help = curent_crosswrord.getElementsByTagName("napoveda")[0].childNodes[0].nodeValue;
+        //console.log(help);
+        var solution = curent_crosswrord.getElementsByTagName("riesenie")[0].childNodes[0].nodeValue;
+        //console.log(solution);
+        for (var i = 0; i < result.length; i++) {
+            var demoHTML = document.getElementById("crossword");
+            /*document.getElementById("demo").innerHTML += xmlDoc.getElementsByTagName("napoveda")[i].childNodes[0].nodeValue+'<br>'+
+                xmlDoc.getElementsByTagName("riesenie")[i].childNodes[0].nodeValue+'<br>';*/
+            var sol1 = curent_crosswrord.getElementsByTagName("riesenie")[i].childNodes[0].nodeValue;
+            var found_match = 0;
+            demoHTML.innerHTML += '<div class = "outer_div"></div>';
+            solutions.push(sol1);
+
+            final_result = result;
+            for (var j = 0; j < sol1.length; j++) {
+                //console.log(sol1[j]);
+                addToArray(sol1[j]);
+                if (result[i] == sol1[j] && found_match != 1) {
+                    //console.log(result[i]);
+                    found_match = 1;
+                    demoHTML.innerHTML += '<div class = "crossword_box" id = "actual_crossword" ' +
+                        'ondrop="drop(event)"' +
+                        ' ondragover="allowDrop(event)"></div>';
+                    //'><div>';
+                }
+                else {
+                    demoHTML.innerHTML += '<div class = "crossword_box"' +
+                        'ondrop="drop(event)"' +
+                        ' ondragover="allowDrop(event)"></div>';
+                    //'></div>';
+                }
+                if (j == sol1.length - 1) {
+                    demoHTML.innerHTML += curent_crosswrord.getElementsByTagName("napoveda")[i].childNodes[0].nodeValue;
+                }
+            }
+            found_match = 0;
+        }
+        // console.log(arrayOfLetters);
+        arrayOfLetters = arrayOfLetters.sort();
+        //console.log(arrayOfLetters);
+        for (var k = 0; k < arrayOfLetters.length; k++) {
+
+            drawLetters(arrayOfLetters[k], k);
+        }
+        document.getElementById("points_div").innerHTML += points
+    }
+}
+function addToArray(a) {
+    //console.log(a);
+    arrayOfLetters.push(a);
 
 }
+
+function drawLetters(a, i) {
+    document.getElementById("letters_div").innerHTML+='<div id="'+a+i+'" title="'+a+'" class="letters_cls" ' +
+        'draggable="true" ondragstart="drag(event)">'+a+'</div>';
+        // '>'+a+'</div>';
+
+}
+
+
